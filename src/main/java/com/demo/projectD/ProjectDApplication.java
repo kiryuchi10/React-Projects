fe854922992fb719dca78f20ae1bdeb5312d1548
kiryuchi10 <kaminer_sol2@naver.com>
2024-07-27 09:02:16 +0900

    240727 commit
    new properties setting for react projects

--- src/main/java/com/demo/projectD/ProjectDApplication.java
+++ src/main/java/com/demo/projectD/ProjectDApplication.java
@@ -1,10 +1,8 @@
 package com.demo.projectD;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.demo.projectD.repository.dao")
 @SpringBootApplication
 public class ProjectDApplication {
 
